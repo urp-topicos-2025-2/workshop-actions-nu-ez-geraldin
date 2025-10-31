364ef6699052d1eec01e19b3cdf8ef86936e1940
Geraldine <146147105+geraldin21l@users.noreply.github.com>
2025-10-31 15:19:38 +0000

    fix: Corregir vulnerabilidad Incomplete URL scheme checks

--- src/components/OctoLink.tsx
+++ src/components/OctoLink.tsx
@@ -1,23 +1,14 @@
-import { FunctionComponent, ReactElement } from "react";
-
-interface OctoLinkProps {
-  href: string;
-  title: string;
-}
-
 function sanitizeUrl(url: string) {
   // UNCOMMENT THE FOLLOWING LINES TO INTRODUCE A SECURITY VULNERABILITY FOR STEP 04: SECURITY
   const u = decodeURI(url).trim().toLowerCase();
-  if (u.startsWith("javascript:")) {
+  
+  if (
+    u.startsWith("javascript:") ||
+    u.startsWith("data:") || // <--- Esta es la corrección
+    u.startsWith("vbscript:") // <--- Esta es la corrección
+  ) {
     return "about:blank";
   }
+  
   return url;
 }
-const OctoLink: FunctionComponent<OctoLinkProps> = ({
-  href,
-  title,
-}): ReactElement => {
-  return <a href={sanitizeUrl(href)}>{title}</a>;
-};
-
-export { OctoLink };
