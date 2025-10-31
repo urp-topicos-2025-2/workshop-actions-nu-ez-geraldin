9b6f6ea8962444dd3072479a1e6f467315b5be97
Geraldine <146147105+geraldin21l@users.noreply.github.com>
2025-10-31 15:07:31 +0000

    Add security vulnerability

--- src/components/OctoLink.tsx
+++ src/components/OctoLink.tsx
@@ -7,13 +7,12 @@ interface OctoLinkProps {
 
 function sanitizeUrl(url: string) {
   // UNCOMMENT THE FOLLOWING LINES TO INTRODUCE A SECURITY VULNERABILITY FOR STEP 04: SECURITY
-  // const u = decodeURI(url).trim().toLowerCase();
-  // if (u.startsWith("javascript:")) {
-  //   return "about:blank";
-  // }
+  const u = decodeURI(url).trim().toLowerCase();
+  if (u.startsWith("javascript:")) {
+    return "about:blank";
+  }
   return url;
 }
-
 const OctoLink: FunctionComponent<OctoLinkProps> = ({
   href,
   title,
